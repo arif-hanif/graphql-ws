4f16efd27778f6443ecab4f45fd72d10e5ada625
Chris Beaven <smileychris@gmail.com>
2018-09-02 00:28:37 +1200

    Cleaner setuptools configuration

--- graphql_ws/__init__.py
+++ graphql_ws/__init__.py
@@ -3,8 +3,8 @@
 """Top-level package for GraphQL WS."""
 
 __author__ = """Syrus Akbary"""
-__email__ = 'me@syrusakbary.com'
-__version__ = '0.1.0'
+__email__ = "me@syrusakbary.com"
+__version__ = "0.2.0"
 
 
 from .base import BaseConnectionContext, BaseSubscriptionServer
